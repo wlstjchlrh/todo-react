140698c72d2704d9e98c40ab40adb597012e3495
wlstjchlrh <127806604+wlstjchlrh@users.noreply.github.com>
2023-05-03 18:40:16 +0900

    todoreact-createdat

---
+++ src/firebase/index.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey:"AIzaSyA1mCG33veH9Pngjhe4rMtB0vHIir7GXdc",
+  authDomain:"wp-firebase-b6e69.firebaseapp.com",
+  projectId:  "wp-firebase-b6e69",
+  storageBucket: "wp-firebase-b6e69.appspot.com",
+  messagingSenderId: "547194047227",
+  appId: "1:547194047227:web:9533100f3203f2ccc9be5e",
+  measurementId:  "G-0BD28S31K4",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { auth, db };
