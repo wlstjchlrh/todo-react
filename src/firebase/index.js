a353764fbe7ed75e658191d954a0d2343d4e96f4
wlstjchlrh <127806604+wlstjchlrh@users.noreply.github.com>
2023-05-05 02:04:46 +0900

    ..

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey:"AIzaSyA1mCG33veH9Pngjhe4rMtB0vHIir7GXdc",
-  authDomain:"wp-firebase-b6e69.firebaseapp.com",
-  projectId:"wp-firebase-b6e69",
-  storageBucket:"wp-firebase-b6e69.appspot.com",
-  messagingSenderId:"547194047227",
-  appId:"1:547194047227:web:9533100f3203f2ccc9be5e",
-  measurementId:"G-0BD28S31K4"
+  apiKey: "AIzaSyBKdRmeMp66l4T_A7HW3Da3KVe6su1XUKw",
+  authDomain: "todofiffffn.firebaseapp.com",
+  projectId: "todofiffffn",
+  storageBucket: "todofiffffn.appspot.com",
+  messagingSenderId: "992667691580",
+  appId: "1:992667691580:web:1bdaaa1008ef1d87d73139",
+  measurementId: "G-06H9SYDLLW"
 };
 
 const app = initializeApp(firebaseConfig);
