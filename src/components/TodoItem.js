17e6d04f5c1862bb6549dd712ec058e237cc0b93
wlstjchlrh <127806604+wlstjchlrh@users.noreply.github.com>
2023-05-09 11:59:09 +0900

    //finf

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -23,7 +23,10 @@ const TodoItem = ({ todo, onToggle, onDelete }) => {
       >
         {todo.text}
       </span>
-      <span className={styles.datetime}>{todo.datetime}</span> 
+      <span className={styles.todoDatetime}>
+        {todo.datetime.substring(0, 10)}
+      </span>
+
       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 
       {/* 삭제 버튼을 렌더링하고, 클릭 시 onDelete 함수를 호출하여 해당 할 일을 삭제합니다. */}
